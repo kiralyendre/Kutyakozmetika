8088c2928bcdd20f7bf78077f26c982f61f8e540
kiralyendre <129405643+kiralyendre@users.noreply.github.com>
2023-12-12 15:44:07 +0100

    Add files via upload
    create project

---
+++ Kutyakozmetika/Kutyakozmetika/src/test/java/com/project/Kutyakozmetika/KutyakozmetikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Kutyakozmetika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KutyakozmetikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
